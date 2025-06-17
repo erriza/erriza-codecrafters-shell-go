d8b39d95119949ae9d2fd09474f7a29105bcebba
ricsae <eduricsa@hotmail.com>
2025-06-17 11:20:58 -0600

    multiple commands

--- app/main.go
+++ app/main.go
@@ -12,14 +12,17 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command,err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("error", err.Error())
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+	
+		// Wait for user input
+		command,err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("error", err.Error())
+			os.Exit(1)
+		}
+		command = strings.TrimSpace(command)
+		fmt.Println(command + ": command not found")
 	}
-	command = strings.TrimSpace(command)
-	fmt.Println(command + ": command not found")
 }
